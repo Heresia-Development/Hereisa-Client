b0ab0b9917277618f2f3ecd8eec58896adf22087
Shivada <mehmeterensariot15@gmail.com>
2026-03-29 12:59:35 +0300

    Create Reach.java

---
+++ src/main/java/com/modules/combat/Reach.java
@@ -0,0 +1,11 @@
+package com.hereisa.client.modules.combat;
+
+import com.hereisa.client.core.Category;
+import com.hereisa.client.core.Module;
+
+public class Reach extends Module {
+    public Reach() {
+        super("Reach", Category.COMBAT);
+        this.reachValue = 4.5f;
+    }
+}
