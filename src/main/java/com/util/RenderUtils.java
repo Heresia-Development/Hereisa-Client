09ed6763e46de4e62a6b1f3f1b4c78d163d5e70c
Shivada <mehmeterensariot15@gmail.com>
2026-03-29 14:43:36 +0300

    Update RenderUtils.java

--- src/main/java/com/util/RenderUtils.java
+++ src/main/java/com/util/RenderUtils.java
@@ -4,15 +4,21 @@ import net.minecraft.client.gui.DrawContext;
 import net.minecraft.util.math.MathHelper;
 
 public class RenderUtils {
-    public static void drawOutlinedBox(DrawContext ctx, int x, int y, int w, int h, int color) {
-        ctx.fill(x, y, x + w, y + 1, color); 
-        ctx.fill(x, y + h - 1, x + w, y + h, color); 
-        ctx.fill(x, y, x + 1, y + h, color); 
-        ctx.fill(x + w - 1, y, x + w, y + h, color); 
+
+    public static void drawOutlinedRectangle(DrawContext ctx, int x, int y, int w, int h, int color, int thickness) {
+        ctx.fill(x, y, x + w, y + thickness, color);
+        ctx.fill(x, y + h - thickness, x + w, y + h, color);
+        ctx.fill(x, y, x + thickness, y + h, color);
+        ctx.fill(x + w - thickness, y, x + w, y + h, color);
     }
 
-    public static int getHealthColor(float h, float m) {
-        float r = MathHelper.clamp(h / m, 0, 1);
-        return (255 << 24) | ((int)(255 * (1-r)) << 16) | ((int)(255 * r) << 8);
+    public static int getHealthColor(float health, float maxHealth) {
+        float ratio = MathHelper.clamp(health / maxHealth, 0, 1);
+        
+        int r = (int) (255 * (1 - ratio));
+        int g = (int) (255 * ratio);
+        int b = 0;
+        
+        return (255 << 24) | (r << 16) | (g << 8) | b;
     }
 }
