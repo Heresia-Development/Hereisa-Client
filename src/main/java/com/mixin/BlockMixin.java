e6f60c2e84ff5c83217e706670b41148f30720ec
Shivada <mehmeterensariot15@gmail.com>
2026-03-29 15:05:50 +0300

    Update BlockMixin.java

--- src/main/java/com/mixin/BlockMixin.java
+++ src/main/java/com/mixin/BlockMixin.java
@@ -2,7 +2,7 @@ package com.hereisa.client.mixin;
 
 import com.hereisa.client.HereisaClient;
 import com.hereisa.client.modules.render.XRay;
-import net.minecraft.block.Block;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
@@ -12,11 +12,11 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(Block.class)
+@Mixin(AbstractBlock.class)
 public abstract class BlockMixin {
 
     @Inject(method = "shouldDrawSide", at = @At("HEAD"), cancellable = true)
-    private static void onShouldDrawSide(BlockState state, BlockView world, BlockPos pos, Direction side, BlockPos otherPos, CallbackInfoReturnable<Boolean> cir) {
+    private void onShouldDrawSide(BlockState state, BlockView world, BlockPos pos, Direction side, BlockPos otherPos, CallbackInfoReturnable<Boolean> cir) {
         
         boolean isXrayActive = HereisaClient.moduleManager.getModules().stream()
                 .anyMatch(m -> m.getName().equalsIgnoreCase("XRay") && m.isEnabled());
@@ -36,7 +36,7 @@ public abstract class BlockMixin {
                 .anyMatch(m -> m.getName().equalsIgnoreCase("XRay") && m.isEnabled());
 
         if (isXrayActive) {
-            cir.setReturnValue(1.0f);
+            cir.setReturnValue(1.0f); 
         }
     }
 }
