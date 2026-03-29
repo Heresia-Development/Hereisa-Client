45e0c57af04a634e55c1c6d6515a0375946fbd42
Shivada <mehmeterensariot15@gmail.com>
2026-03-29 14:47:18 +0300

    Update InGameHudMixin.java

--- src/main/java/com/mixin/InGameHudMixin.java
+++ src/main/java/com/mixin/InGameHudMixin.java
@@ -4,6 +4,7 @@ import com.hereisa.client.HereisaClient;
 import com.hereisa.client.modules.render.ESP;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.render.RenderTickCounter;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,14 +12,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(InGameHud.class)
 public class InGameHudMixin {
+
     @Inject(method = "render", at = @At("RETURN"))
-    private void onRender(DrawContext context, float tickDelta, CallbackInfo ci) {
+    private void onRender(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci) {
         HereisaClient.moduleManager.getModules().stream()
             .filter(m -> m instanceof ESP)
             .map(m -> (ESP) m)
             .forEach(esp -> {
                 if (esp.isEnabled()) {
-                    esp.onRender2D(context);
+                    esp.onRender2D(context, tickCounter.getTickDelta(false));
                 }
             });
     }
