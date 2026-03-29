0b6a2b5dbc02a0ee15af9e11048d3fc17dc9d9f1
Shivada <mehmeterensariot15@gmail.com>
2026-03-29 13:14:14 +0300

    Create InGameHudMixin.java

---
+++ src/main/java/com/mixin/InGameHudMixin.java
@@ -0,0 +1,25 @@
+package com.hereisa.client.mixin;
+
+import com.hereisa.client.HereisaClient;
+import com.hereisa.client.modules.render.ESP;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.hud.InGameHud;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(InGameHud.class)
+public class InGameHudMixin {
+    @Inject(method = "render", at = @At("RETURN"))
+    private void onRender(DrawContext context, float tickDelta, CallbackInfo ci) {
+        HereisaClient.moduleManager.getModules().stream()
+            .filter(m -> m instanceof ESP)
+            .map(m -> (ESP) m)
+            .forEach(esp -> {
+                if (esp.isEnabled()) {
+                    esp.onRender2D(context);
+                }
+            });
+    }
+}
