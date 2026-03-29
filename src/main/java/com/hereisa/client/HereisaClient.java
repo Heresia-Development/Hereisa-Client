c4910409ada849625f55871e17c50205edae63e7
Shivada <mehmeterensariot15@gmail.com>
2026-03-29 15:59:35 +0300

    Update HereisaClient.java

--- src/main/java/com/hereisa/client/HereisaClient.java
+++ src/main/java/com/hereisa/client/HereisaClient.java
@@ -5,6 +5,7 @@ import com.hereisa.client.ui.ClickGUI;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
 
 public class HereisaClient implements ClientModInitializer {
     public static ModuleManager moduleManager;
@@ -12,11 +13,16 @@ public class HereisaClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         moduleManager = new ModuleManager();
+
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if (client.player != null) {
+            if (client.player != null && client.world != null) {
                 moduleManager.onTick();
-                if (InputUtil.isKeyPressed(client.getWindow().getHandle(), 344)) { 
-                    if (!(client.currentScreen instanceof ClickGUI)) client.setScreen(new ClickGUI());
+
+                long windowHandle = client.getWindow().getHandle();
+                if (InputUtil.isKeyPressed(windowHandle, GLFW.GLFW_KEY_RIGHT_SHIFT)) {
+                    if (!(client.currentScreen instanceof ClickGUI)) {
+                        client.setScreen(new ClickGUI());
+                    }
                 }
             }
         });
