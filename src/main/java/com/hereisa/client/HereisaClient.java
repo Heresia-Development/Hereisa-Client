23dd07fb9ef005664ddba082c1fddac12b7509b4
Shivada <mehmeterensariot15@gmail.com>
2026-03-29 16:05:15 +0300

    Update HereisaClient.java

--- src/main/java/com/hereisa/client/HereisaClient.java
+++ src/main/java/com/hereisa/client/HereisaClient.java
@@ -4,22 +4,33 @@ import com.hereisa.client.core.ModuleManager;
 import com.hereisa.client.ui.ClickGUI;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
 
 public class HereisaClient implements ClientModInitializer {
     public static ModuleManager moduleManager;
+    private static KeyBinding guiKeyBinding;
 
     @Override
     public void onInitializeClient() {
         moduleManager = new ModuleManager();
 
+        guiKeyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.hereisa.clickgui", 
+                InputUtil.Type.KEYSYM, 
+                GLFW.GLFW_KEY_RIGHT_SHIFT, 
+                "category.hereisa.client"
+        ));
+
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             if (client.player != null && client.world != null) {
-                moduleManager.onTick();
+                if (moduleManager != null) {
+                    moduleManager.onTick();
+                }
 
-                long windowHandle = client.getWindow().getHandle();
-                if (InputUtil.isKeyPressed(windowHandle, GLFW.GLFW_KEY_RIGHT_SHIFT)) {
+                while (guiKeyBinding.wasPressed()) {
                     if (!(client.currentScreen instanceof ClickGUI)) {
                         client.setScreen(new ClickGUI());
                     }
